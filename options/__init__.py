3d6bf88bcd0b63ca38be42839dc20181a8b15a33
Mike Turner <mwtvy4@mst.edu>
2019-06-04 07:37:32 -0700

    initialized python package

---
+++ options/__init__.py
@@ -0,0 +1,6 @@
+def bsPut():
+    return 1
+def bsCall():
+    return 1
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='options',
+    version='0.1',
+    description='options tools for trading',
+    url='github.com/MiataMike/Options-platform',
+    author='Michael Turner',
+    license='GNU GPLv3',
+    packages=['options'],
+    zip_safe=False)
